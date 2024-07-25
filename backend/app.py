20b93340730ea981b7cb4fa85723a446395d1dbf
Oussama Rafouk <156216265+ocryptocode@users.noreply.github.com>
2024-07-25 09:33:15 +0100

    add the backend

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from routes import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/db_name'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run(debug=True)
